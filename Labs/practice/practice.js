d1babba51d0688dc34a291274bdd248fb162cc91
CSchoon25 <132482430+CSchoon25@users.noreply.github.com>
2023-06-29 08:46:14 -0400

    updated practice page

---
+++ Labs/practice/practice.js
@@ -0,0 +1,17 @@
+function setup() {
+    var red = "#FF0000"
+
+    createCanvas(800, 600)
+    noStroke()
+
+    background(237, 210, 135)
+
+    fill(111, 208, 232)
+    rect(30, 60, 300, 150)
+
+    fill(200, 0, 0)
+    circle(100, 100, 30)
+
+    stroke(2)
+    line(0, 500, 200, 20)
+}
